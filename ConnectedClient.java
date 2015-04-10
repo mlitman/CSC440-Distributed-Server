76d3334672684854aa06911af664ac5724a12fb2
Mike Litman <mike@awesomefat.com>
2015-04-10 16:03:59 -0500

    made more changes

--- ConnectedClient.java
+++ ConnectedClient.java
@@ -1,5 +1,6 @@
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -8,7 +9,7 @@ public class ConnectedClient
 	private Socket theSocket;
 	private PrintWriter output;
 	private Scanner input;
-	
+	private boolean[] myBytes;
 	public ConnectedClient(Socket theSocket)
 	{
 		try
@@ -23,6 +24,30 @@ public class ConnectedClient
 		}
 	}
 	
+	public boolean hasByte(int pos)
+	{
+		return this.myBytes[pos];
+	}
+	
+	public void initBytes(int size, boolean b)
+	{
+		this.myBytes = new boolean[size];
+		Arrays.fill(this.myBytes, b);
+	}
+	
+	public void displayBytes()
+	{
+		for(boolean b : this.myBytes)
+		{
+			System.out.println(b);
+		}
+	}
+	
+	public void addedByte(int index)
+	{
+		this.myBytes[index] = true;
+	}
+	
 	public void sendMessage(String msg)
 	{
 		this.output.println(msg);
@@ -31,5 +56,10 @@ public class ConnectedClient
 	public String getMessage()
 	{
 		return this.input.nextLine();
+	}
+	
+	public int readByte() throws Exception
+	{
+		return this.theSocket.getInputStream().read();
 	}
 }
--- Driver.java
+++ Driver.java
@@ -8,9 +8,12 @@ import java.util.Scanner;
 public class Driver 
 {
 	static LinkedList<ConnectedClient> theClients = new LinkedList<ConnectedClient>();
+	static LinkedList<ConnectedClient> theBusyClients = new LinkedList<ConnectedClient>();
+	static LinkedList<ConnectedClient> theNotBusyClients = new LinkedList<ConnectedClient>();
 	
 	public static void main(String[] args) throws Exception
 	{
+		//******Server Code******
 		ServerSocket ss = new ServerSocket(1234);
 		while(true)
 		{
@@ -18,6 +21,7 @@ public class Driver
 			Socket connection = ss.accept();
 			ConnectedClient cc = new ConnectedClient(connection);
 			Driver.theClients.add(cc);
+			Driver.theNotBusyClients.add(cc);
 			ResponseThread rt = new ResponseThread(cc);
 			rt.start();
 			System.out.println("Connected!");
